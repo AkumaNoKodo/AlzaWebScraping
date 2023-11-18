e436ed79b81207e71c145857d00dcb800550e755
NoGamesNoLife <markus@e-vanicek.cz>
2023-11-18 15:45:06 +0100

    Documentation

--- AlzaScraper.py
+++ AlzaScraper.py
@@ -2,10 +2,7 @@ import json
 import pandas as pd
 import requests
 from datetime import datetime
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from tutorial.tutorial.spiders.alza_spider import AlzaProductSpider
@@ -15,54 +12,75 @@ class AlzaProductParametersExtractor:
     """ Class for extracting product parameters from the Alza.cz website."""
 
     def __init__(self):
-        """ Initializes the class with file names for storing product links and data. """
+        """
+        Initializes the AlzaProductParametersExtractor class.
+
+        Sets up the filenames for storing product links and extracted data. The product
+        links are stored in 'products.txt', and the extracted data is stored in 'data.json'.
+        """
         self.product_file = 'products.txt'
         self.data_file = 'data.json'
 
-    def extract_and_save_products(self, category_url: str, number_of_pages: int):
-        """ Extracts product links from a given category and saves them to a file. """
+    def extract_and_save_products(self, category_url: str, number_of_pages: int) -> None:
+        """
+        Extracts product links from a specified category URL and saves them into a file.
+
+        This method uses a Scrapy CrawlerProcess to crawl a given category URL for a specified
+        number of pages. It saves the extracted product links to a text file.
+
+        Args:
+            category_url (str): The URL of the category to scrape.
+            number_of_pages (int): The number of pages to crawl in the category.
+        """
         process = CrawlerProcess(get_project_settings())
         process.crawl(AlzaProductSpider,
                       target=category_url,
                       number_of_pages=number_of_pages,
                       file_name=self.product_file)
         process.start()
-        # product_links = self.get_products(category_url, number_of_pages)
-        # with open(self.product_file, 'w') as file:
-        #     for link in product_links:
-        #         file.write(link + '\n')
 
-    def process_products(self):
-        """ Processes each product, extracts parameters, and saves them to a data file. """
+    def process_products(self) -> None:
+        """
+        Processes each product URL, extracts parameters, and saves them to a JSON file.
+
+        Reads product URLs from a file, extracts parameters for each product, and appends
+        them to a JSON file. Each product's parameters are extracted using the 'get_parameters'
+        method and stored in a dictionary format.
+        """
         with open(self.data_file, 'a') as data_file, open(self.product_file, 'r') as file:
             lines = file.readlines()
             for idx, line in enumerate(lines, 1):
                 product_url = line.strip()
                 print(f"Processing product {idx}/{len(lines)}: {product_url}")
-                parameters = self.get_parameters(product_url)
+                parameters = self._get_parameters(product_url)
                 json.dump(parameters, data_file)
                 data_file.write('\n')
 
-    def convert_json_to_csv(self):
-        """ Converts the JSON data file to a CSV format. """
+    def convert_json_to_csv(self) -> None:
+        """
+        Converts the JSON data file into a CSV format.
+
+        Reads the JSON file containing product data, filters out duplicate entries based on all
+        columns except 'Scraping time', and saves the result into a CSV file named 'data.csv'.
+        """
         df = pd.read_json(self.data_file, lines=True)
         df_filtered = df.drop_duplicates(subset=[col for col in df.columns if col != 'Scraping time'])
         df_filtered.to_csv('data.csv', index=False, sep=';')
 
-    # def get_products(self, category_url: str, number_of_pages: int) -> list:
-    #     """ Retrieves product links from specified pages of a category. """
-    #     all_links = []
-    #     links = [category_url + f"#pg={x}" for x in range(1, number_of_pages + 1)]
-    #     for link in links:
-    #         try:
-    #             product_urls = self.get_product_url_list(link)
-    #             all_links.extend(product_urls)
-    #         except Exception as e:
-    #             print(f"Error fetching page {link}: {e}")
-    #     return all_links
+    def _get_parameters(self, product_url: str) -> dict:
+        """
+        Extracts product parameters from a given product URL.
 
-    def get_parameters(self, product_url: str) -> dict:
-        """ Extracts product parameters from a given product URL. """
+        Visits the product page and extracts various parameters like price, availability,
+        and other product-specific attributes. The method organizes these parameters into
+        a dictionary.
+
+        Args:
+            product_url (str): The URL of the product page to extract data from.
+
+        Returns:
+            dict: A dictionary containing extracted product parameters.
+        """
         row_elements, price, availability = self._extract_rows_and_price(product_url)
         parameters = {
             "Scraping time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -78,18 +96,21 @@ class AlzaProductParametersExtractor:
 
         return parameters
 
-    # @staticmethod
-    # def get_product_url_list(target_url: str) -> list:
-    #     """ Extracts and returns a list of product URLs from the target page. """
-    #     base_url = 'https://www.alza.cz'
-    #     response = requests.get(target_url)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     links = soup.find_all('a', class_='name browsinglink')
-    #     return [base_url + link['href'] + "#parametry" for link in links]
-
     @staticmethod
-    def _extract_rows_and_price(product_url: str) -> (list, str, str):
-        """ Extracts detailed rows, price, and availability info from a product page. """
+    def _extract_rows_and_price(product_url: str) -> tuple[list[Tag], str, str]:
+        """
+        Extracts detailed rows, price, and availability information from a product page.
+
+        This method sends a request to the product URL and parses the HTML response to
+        extract product details, price, and availability information.
+
+        Args:
+            product_url (str): The URL of the product page.
+
+        Returns:
+            tuple: A tuple containing a list of detail rows, the price, and the availability
+                   information of the product.
+        """
         response = requests.get(product_url)
         soup = BeautifulSoup(response.text, 'html.parser')
         rows = soup.find_all('div', class_='row')
@@ -97,8 +118,19 @@ class AlzaProductParametersExtractor:
         return rows, price, availability
 
     @staticmethod
-    def _parse_price_and_availability(soup):
-        """ Parses and returns price and availability information from the soup object. """
+    def _parse_price_and_availability(soup: BeautifulSoup) -> tuple[str, str]:
+        """
+        Parses and returns price and availability information from a BeautifulSoup object.
+
+        Extracts the price and availability details from the parsed HTML content of the product
+        page.
+
+        Args:
+            soup (bs4.BeautifulSoup): A BeautifulSoup object of the product page.
+
+        Returns:
+            tuple: A tuple containing the extracted price and availability information.
+        """
         price_detail_block = soup.find('div', class_='price-detail')
         price = None
         if price_detail_block:
@@ -110,7 +142,19 @@ class AlzaProductParametersExtractor:
         availability = availability_span.get_text(strip=True) if availability_span else "Unknown"
         return price, availability
 
-    def _extract_name_and_value(self, row):
+    def _extract_name_and_value(self, row: Tag) -> tuple[str, str]:
+        """
+        Filters and returns elements of interest from a given row.
+
+        Iterates through specified CSS classes to find elements within the row and removes
+        any unwanted elements like popups or dialogs before returning them.
+
+        Args:
+            row (bs4.element.Tag): A BeautifulSoup element representing a row in the HTML structure.
+
+        Returns:
+            list: A list of BeautifulSoup elements after filtering out unwanted parts.
+        """
         filtered_elements = self._filtered_elements(row)
         name, value = "", ""
         for elem in filtered_elements:
@@ -121,12 +165,24 @@ class AlzaProductParametersExtractor:
         return name, value
 
     @staticmethod
-    def _filtered_elements(row) -> list:
-        result = []
+    def _filtered_elements(row: Tag) -> list[Tag]:
+        """
+        Filters and returns elements of interest from a given row.
+
+        Iterates through specified CSS classes to find elements within the row and removes
+        any unwanted elements like popups or dialogs before returning them.
+
+        Args:
+            row (bs4.element.Tag): A BeautifulSoup element representing a row in the HTML structure.
+
+        Returns:
+            list: A list of BeautifulSoup elements after filtering out unwanted parts.
+        """
+        filtered_elements = []
         for class_name in ["name typeName first hasPopupInfo", "value", "name typeName first"]:
             elements = row.find_all(class_=class_name)
             for elem in elements:
                 for unwanted in elem.find_all(class_="infoPopup infoDialog"):
                     unwanted.decompose()
-                result.append(elem)
-        return result
+                filtered_elements.append(elem)
+        return filtered_elements
--- tutorial/tutorial/spiders/alza_spider.py
+++ tutorial/tutorial/spiders/alza_spider.py
@@ -1,24 +1,41 @@
-import json
-from datetime import datetime
 import scrapy
-from scrapy.http import Response
 
 
 class AlzaProductSpider(scrapy.Spider):
+    """
+    Spider for crawling the Alza.cz website to extract product URLs.
+
+    Attributes:
+        name (str): Name of the spider.
+        base_url (str): Base URL of the Alza.cz website.
+        product_file (str): File path where product URLs will be saved.
+    """
+
     name = "alza_products"
     base_url = 'https://www.alza.cz'
 
-    def __init__(self, target: str, number_of_pages: int, file_name: str, *args, **kwargs):
+    def __init__(self, target: str, number_of_pages: int, out_product_file_name: str, *args, **kwargs):
+        """
+        Initializes the spider with the target URL, number of pages, and output file name.
+
+        Args:
+            target (str): The target category URL.
+            number_of_pages (int): Number of pages to scrape in the category.
+            out_product_file_name (str): File path to save the extracted product URLs.
+        """
         super(AlzaProductSpider, self).__init__(*args, **kwargs)
-        self.product_file = file_name
-        self.target = target
-        self.number_of_pages = number_of_pages
+        self.product_file = out_product_file_name
+        self.start_urls = [f"{target}-p{page}.htm" for page in range(1, number_of_pages + 1)]
 
-    def start_requests(self):
-        for page in range(1, self.number_of_pages + 1):
-            yield scrapy.Request(url=f"{self.target}-p{page}.htm", callback=self.parse)
+    def parse(self, response) -> None:
+        """
+        Parses the product page and extracts product URLs.
 
-    def parse(self, response):
+        Args:
+            response (scrapy.http.Response): Response object from Scrapy.
+
+        Writes each extracted product URL to the specified file.
+        """
         links = response.css('a.name.browsinglink::attr(href)').getall()
         with open(self.product_file, 'a') as file:
             for link in links:
